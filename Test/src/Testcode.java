10958e5fb2aa116cdff213fff5fec171d2165433
melvint31 <mel@abc.com>
2021-10-08 16:03:38 +0000

    another test

--- Test/src/Testcode.java
+++ Test/src/Testcode.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Jav no hello world");  
+     System.out.println("Hello no hello world");  
     }  
 }  
